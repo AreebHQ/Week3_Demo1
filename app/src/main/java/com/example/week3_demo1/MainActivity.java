8d381d9fa7246187e236f49c78ce0528aad9defa
Areeb Hussain <areeb_hq@yahoo.com>
2021-09-17 20:41:21 -0400

    added sum of 2

--- app/src/main/java/com/example/week3_demo1/MainActivity.java
+++ app/src/main/java/com/example/week3_demo1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.week3_demo1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +23,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Intent recInt = getIntent();
+
+
+
+
         mTextView = (TextView)findViewById(R.id.tv1);
+        mTextView.setText(recInt.getIntExtra("THESUM",0));
 
 
         btn1 = (Button)findViewById(R.id.btn1);
